91f3a2b8f299a0de4e5d4cc25d0769412bddf420
Todd Carlson <todd.carlson3030@me.com>
2019-04-10 07:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-add() 
-subtract()
-multiply()
-divide()
+add(a, b) {
+  return a + b;
+}
+
+subtract(a, b) {
+  return a - b;
+}
+
+multiply(a, b) {
+  return a * b;
+}
+divide(a, b) {
+  return a / b;
+}
 
